bc89b1a8bf8652b90cf800309c1b5a9e6f603f09
jreston <jedi.reston@gritlab.ax>
2025-10-22 14:59:09 +0300

    spring init

---
+++ src/main/java/ax/gritlab/buy_01/Buy01Application.java
@@ -0,0 +1,13 @@
+package ax.gritlab.buy_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buy01Application.class, args);
+	}
+
+}
---
+++ src/test/java/ax/gritlab/buy_01/Buy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ax.gritlab.buy_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
